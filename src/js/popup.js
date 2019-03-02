5ba1ad17471ad7fa5646a5ce068249e1b67af270
toshi <k08045kk+toshi@gmail.com>
2019-03-03 08:07:00 +0900

    update ポップアップページ変更

--- src/js/popup.js
+++ src/js/popup.js
@@ -2,48 +2,68 @@
  * ポップアップページ処理
  */
 
-// ページ読み込み完了イベント
-function onPageLoaded() {
-  // テキスト読込み(国際化)
-  document.querySelectorAll('*[data-label]').forEach(function(v, i, a) {
-    v.textContent = chrome.i18n.getMessage(v.dataset.label);
-  });
-}
-onPageLoaded();
-
-// アクション設定
+// コピー完了イベント
 function onTabCopyComplete() {
   window.close();
 }
-['CopyTabTitleUrl', 'CopyTabTitle', 'CopyTabUrl', 'CopyTabFormat'].forEach(function(v, i, a) {
-  document.getElementById('item_'+v).addEventListener('click', function() {
-    onCopyTabs(i, {currentWindow:true, active:true}, null, onTabCopyComplete);
-  });
-  document.getElementById('current_'+v).addEventListener('click', function() {
-    onCopyTabs(i, {currentWindow:true}, null, onTabCopyComplete);
-  });
-  document.getElementById('all_'+v).addEventListener('click', function() {
-    onCopyTabs(i, {}, null, onTabCopyComplete);
-  });
-});
 
-// アクション(ポップアップ表示なし時)
-getStorageArea().get(defaultStorageValueSet, function(valueSet) {
-  if (valueSet.action == 'Action') {
-    // 2重にストレージを取得できないため、copyTabs関数を直呼びする
-    let targetSet = {CurrentTab: {currentWindow:true, active:true}, 
-                      CurrentWindow: {currentWindow:true}, 
-                      AllWindow: {}};
-    let actionSet = {CopyTabTitleUrl:0, CopyTabTitle:1, CopyTabUrl:2, CopyTabFormat:3};
-    let type = actionSet[valueSet.action_action];
-    let query = targetSet[valueSet.action_target];
+// ページ読み込み完了イベント
+function onInit() {
+  // テキスト読込み(国際化)
+  //document.querySelectorAll('*[data-label]').forEach(function(v, i, a) {
+  //  v.textContent = chrome.i18n.getMessage(v.dataset.label);
+  //});
+  
+  // チェックボックスイベント設定
+  function onClickCheckbox() {
+    let win = document.getElementById('target_win');
+    let all = document.getElementById('target_all');
     
-    let msg = chrome.i18n.getMessage('optionsPage_CopyComplated');
-    document.querySelector('.action p').innerText = msg;
-    document.querySelector('.action').style.display = 'block';
-    document.querySelector('.panel').style.display = 'none';
-    onCopyTabs(type, query, valueSet, function() {
-      setTimeout(onTabCopyComplete, 1000);
-    });
+    if (this.id == 'target_win_label' && !win.checked && all.checked) {
+      all.checked = false;
+    }
+    if (this.id == 'target_all_label' && !all.checked && win.checked) {
+      win.checked = false;
+    }
   }
-});
+  document.getElementById('target_win_label').addEventListener('click', onClickCheckbox);
+  document.getElementById('target_all_label').addEventListener('click', onClickCheckbox);
+  
+  // コピーイベント設定
+  ['CopyTabTitleUrl', 'CopyTabTitle', 'CopyTabUrl', 'CopyTabFormat'].forEach(function(v, i, a) {
+    document.getElementById('item_'+v).addEventListener('click', function() {
+      let win = document.getElementById('target_win');
+      let all = document.getElementById('target_all');
+      let target = {currentWindow:true, active:true};
+      if (win.checked) {  target = {currentWindow:true};  }
+      if (all.checked) {  target = {};  }
+      
+      onCopyTabs(i, target, null, onTabCopyComplete);
+    });
+  });
+  
+  // アクション
+  getStorageArea().get(defaultStorageValueSet, function(valueSet) {
+    if (valueSet.action == 'Action') {
+      // アクションのみ(完了通知を表示する)
+      let targetSet = {CurrentTab: {currentWindow:true, active:true}, 
+                        CurrentWindow: {currentWindow:true}, 
+                        AllWindow: {}};
+      let actionSet = {CopyTabTitleUrl:0, CopyTabTitle:1, CopyTabUrl:2, CopyTabFormat:3};
+      let type = actionSet[valueSet.action_action];
+      let query = targetSet[valueSet.action_target];
+      
+      onCopyTabs(type, query, valueSet, function() {
+        setTimeout(onTabCopyComplete, 1000);
+      });
+      document.getElementById('action').style.display = '';
+    } else {
+      // ポップアップを表示する
+      document.getElementById('panel').style.display = '';
+    }
+  });
+}
+
+(function main() {
+  document.addEventListener("DOMContentLoaded", onInit);
+})();
